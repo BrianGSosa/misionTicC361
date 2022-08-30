1b11e33ac6959271750d1d63b309a17627c8d144
BrianGSosa <dante2202@hotmail.com>
2022-08-29 21:29:39 -0500

    SecondCommit

--- ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -3,6 +3,7 @@ namespace ProyectoCiclo3.App.Dominio{
     public class Aviones{
         public int id { get; set; }
         public string modelo { get; set; }
+        public string modelo2 { get; set; }
     }
  
 }
